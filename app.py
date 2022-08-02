59eaa9a09cbec16ef09aa9ba01ac219735850968
shakti-sarada <83201466+shakti-sarada@users.noreply.github.com>
2022-08-02 11:07:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 st.set_option('deprecation.showfileUploaderEncoding', False)
 # Load the pickled model
-pickle_in = open("finalized_modelKNN.pkl","rb")
-model=pickle.load(pickle_in)
+model = pickle.load(open('svmmodel_rbf.pkl', 'rb')) 
+model_randomforest = pickle.load(open('randomforest.pkl', 'rb')) 
 dataset= pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, [2, 3]].values
 from sklearn.preprocessing import StandardScaler
@@ -22,32 +22,58 @@ def predict_note_authentication(UserID, Gender,Age,EstimatedSalary):
     prediction="Item will not be purchased"
   print(prediction)
   return prediction
+def predict_random(UserID, Gender,Age,EstimatedSalary):
+  output= model_randomforest.predict(sc.transform([[Age,EstimatedSalary]]))
+  print("Purchased", output)
+  if output==[1]:
+    prediction="Item will be purchased"
+  else:
+    prediction="Item will not be purchased"
+  print(prediction)
+  return prediction
 def main():
     
     html_temp = """
-   <div class="" style="background-color:grey" >
-   <div class="clearfix">            
+   <div class="" style="background-color:blue;" >
+   <div class="clearfix">           
    <div class="col-md-12">
    <center><p style="font-size:40px;color:white;margin-top:10px;">Poornima Institute of Engineering & Technology</p></center> 
    <center><p style="font-size:30px;color:white;margin-top:10px;">Department of Computer Engineering</p></center> 
-   <center><p style="font-size:25px;color:white;margin-top:10px;">Internship Project Deployment</p></center> 
+   <center><p style="font-size:25px;color:white;margin-top:10px;"Machine Learning Lab Experiment</p></center> 
    </div>
    </div>
    </div>
    """
     st.markdown(html_temp,unsafe_allow_html=True)
-    st.header("Item Purchase Prediction")
+    st.header("Item Purchase Prediction using SVM Algorithm (RBF Kernel Function) and Random Forest Algorithm")
     UserID = st.text_input("UserID","")
-    Gender = st.selectbox('Gender',('Male', 'Female'))
-    Age = st.number_input("Insert Age",18,70)
-    EstimatedSalary = st.number_input("Insert salary",15000,200000)
+    Gender = st.selectbox(
+    "Gender",
+    ("Male", "Female", "Others")
+    )
+    
+    Age = st.number_input('Insert a Age',18,60)
+    #Age = st.text_input("Age","Type Here")
+    EstimatedSalary = st.number_input("Insert EstimatedSalary",15000,150000)
     resul=""
-    if st.button("Predict"):
+    if st.button("SVM RBF Function Prediction"):
       result=predict_note_authentication(UserID, Gender,Age,EstimatedSalary)
-      st.success('Model has predicted {}'.format(result))
+      st.success('SVM Model has predicted {}'.format(result))
+    if st.button("Random Forest Prediction"):
+      result=predict_random(UserID, Gender,Age,EstimatedSalary)
+      st.success('Random forest Model  has predicted {}'.format(result))  
     if st.button("About"):
-      st.subheader("Developed by Shakti Sarada Prasad")
-      st.subheader("Student , Department of Computer Engineering")
-
+      st.header("Developed by Shakti Sarada Prasad")
+      st.subheader("Student , Department of Computer Science Engineering")
+    html_temp = """
+    <div class="" style="background-color:orange;" >
+    <div class="clearfix">           
+    <div class="col-md-12">
+    <center><p style="font-size:20px;color:white;margin-top:10px;">Machine Learning Experiment 5: Support Vector Machine and Random Forest</p></center> 
+    </div>
+    </div>
+    </div>
+    """
+    st.markdown(html_temp,unsafe_allow_html=True)
 if __name__=='__main__':
   main()
